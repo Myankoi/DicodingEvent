b52733deb9c85ce85af083a3114dc38c99c4cf06
Myankoi <dduckh3h3@gmail.com>
2025-03-14 22:04:06 +0700

    set status to always light

--- app/src/main/java/com/example/dicodingevent/ui/MainActivity.kt
+++ app/src/main/java/com/example/dicodingevent/ui/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.dicodingevent.ui
 
 import android.os.Bundle
-import android.view.WindowContentFrameStats
-import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.graphics.toArgb
 import androidx.core.view.WindowInsetsControllerCompat
 import com.example.dicodingevent.navigation.AppNavigation
 import com.example.dicodingevent.ui.theme.DicodingEventTheme
