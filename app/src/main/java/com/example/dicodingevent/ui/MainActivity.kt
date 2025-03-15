5061850c51bcba46d2d63258f84e7abd3c89c188
Myankoi <muhammadramadian079@gmail.com>
2025-03-15 23:18:23 +0700

    add requestNotificationPermission method

--- app/src/main/java/com/example/dicodingevent/ui/MainActivity.kt
+++ app/src/main/java/com/example/dicodingevent/ui/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.dicodingevent.ui
 
-import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.example.dicodingevent.data.local.datastore.SettingPreferences
 import com.example.dicodingevent.navigation.AppNavigation
@@ -19,8 +22,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             val isDarkModeActive by SettingPreferences(this).getThemeSetting().collectAsState(false)
             DicodingEventTheme(dynamicColor = false, darkTheme = isDarkModeActive) {
-                WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = false
-                AppNavigation(isDarkModeActive = isDarkModeActive)
+                WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars =
+                    false
+                AppNavigation(isDarkModeActive = isDarkModeActive, intent = intent)
+                requestNotificationPermission()
+            }
+
+        }
+    }
+
+    private fun requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                    101
+                )
             }
         }
     }
